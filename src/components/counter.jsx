0b6f17dc592e0c5f0817f325960815565f97f54e
Blessing Wangoi <wmugo@kabarak.ac.ke>
2024-11-07 17:18:44 +0300

    Binding Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,38 +7,42 @@ class Counter extends Component {
   
   };
 
-renderTags(){
-  if(this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-  return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+
+// constructor(){
+//   super();
+//   this.handleIncrement =this.handleIncrement.bind(this);
+// }
+
+
+handleIncrement = () => {
+  console.log("Increment Clicked", this);
 }
-
 
   render() { 
 
     return(
      <div>
      
-      {/* <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button className="btn btn-secondary btn-sm">Increment</button> */}
+      <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
 
-      {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()} 
+     
       
       </div>
     );
   }
 
-//   getBadgeClasses() {
-//     let classes = "badge m-2 bg-";
-//     classes += (this.state.count === 0) ? "warning" : "primary";
-//     return classes;
-//   }
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
 
-// formatCount(){
-//   const{ count } =this.state;
-//   return count === 0 ?  "Zero" : count;
-// }
+formatCount(){
+  const{ count } =this.state;
+  return count === 0 ?  "Zero" : count;
+}
 
 }
  
